d46ac9fe03bdd270f24ac4db8cd3e3b1e3cd5692
ramo <knht31i@gmail.com>
2021-02-09 22:08:42 +0900

    hozon

--- analytics.py
+++ analytics.py
@@ -24,33 +24,34 @@ def date_conv(date_str):
 if __name__ == '__main__':
 
     df = pd.read_pickle('tweet_obj.pkl')
+    df.to_csv("employee.csv")
 
     # print(df_tweet[0])
     # print(df[['created_at', 'user.name']].head(1))
 
-    # 日付をdatetime型に変換する処理
-    for index, row in df.iterrows():
-        df.at[index, 'created_at'] = date_conv(row['created_at'])
-    df['date'] = pd.to_datetime(df['created_at'])
+    # # 日付をdatetime型に変換する処理
+    # for index, row in df.iterrows():
+    #     df.at[index, 'created_at'] = date_conv(row['created_at'])
+    # df['date'] = pd.to_datetime(df['created_at'])
 
-    # 出現回数集計用の数字の用意
-    df['count'] = 1
+    # # 出現回数集計用の数字の用意
+    # df['count'] = 1
 
-    # print(df.columns.values)
-    # print(df[['created_at', 'user.name', 'count', 'date']].head(20))
-    # print(df[['user.followers_count', 'user.friends_count', 'user.name', 'date']].head())
+    # # print(df.columns.values)
+    # # print(df[['created_at', 'user.name', 'count', 'date']].head(20))
+    # print(df[['user.followers_count', 'user.friends_count', 'user.name', 'text']].head())
 
-    # 時間帯ごとのルイートの集計
-    tweet_per_hour = df[['count']].groupby(df['date'].dt.hour).sum()
-    tmp = pd.DataFrame({'count': 0}, index=[0])
-    for nindex in range(0, 24):
-        if not nindex in tweet_per_hour.index.tolist():
-            tmp = pd.DataFrame({'count': 0}, index=[nindex])
-            tweet_per_hour = pd.concat([tweet_per_hour, tmp])
-    tweet_per_hour = tweet_per_hour.sort_index()
-    # print(tweet_per_hour.index)
+    # # 時間帯ごとのルイートの集計
+    # tweet_per_hour = df[['count']].groupby(df['date'].dt.hour).sum()
+    # tmp = pd.DataFrame({'count': 0}, index=[0])
+    # for nindex in range(0, 24):
+    #     if not nindex in tweet_per_hour.index.tolist():
+    #         tmp = pd.DataFrame({'count': 0}, index=[nindex])
+    #         tweet_per_hour = pd.concat([tweet_per_hour, tmp])
+    # tweet_per_hour = tweet_per_hour.sort_index()
+    # # print(tweet_per_hour.index)
 
-    # 時間帯ごとのツイート数の可視化
+    # # 時間帯ごとのツイート数の可視化
     # tweet_per_hour.plot.bar()
     # plt.xlabel('hour', size=12)  # x軸指定
     # plt.ylabel('count', size=12)  # Y軸指定
--- gettweet.py
+++ gettweet.py
@@ -18,19 +18,43 @@ if __name__ == '__main__':
 
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    target = "@jishukuchan"
-    # # target = "@saaaaaacchannn"
+    # target = "@jishukuchan"
+    targets = []
 
-    tweets = api.user_timeline(target, count=100)
+    with open('user.csv') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[2] == "woman":
+                targets.append(row[0])
+    f.close()
 
-    tweets_raw = []
-    for tweet in tweets:
-        # print(type(tweet._json))
-        # tweet._json["created_at"] =
-        tweets_raw.append(tweet._json)
+    print(len(targets))
 
-    df = pd.io.json.json_normalize(tweets_raw)
+    tweets_all = []
+    cou = 1
+    for target in targets:
+        print(cou, "/", len(targets), target, end="")
+        tweet_cou = 0
+        for page in range(5):
+            try:
+                tweets = api.user_timeline(target, count=200, page=page)
+                for tweet in tweets:
+                    # print(type(tweet._json))
+                    # tweet._json["created_at"] =
+                    tweets_all.append(tweet._json)
+                    tweet_cou += 1
 
-    print(df)
+                    # print(tweet._json["text"])
+            except tweepy.error.TweepError:
+                continue
+
+        print(" " + str(tweet_cou) + "件取得しました。")
+
+        cou += 1
+
+    df = pd.io.json.json_normalize(tweets_all)
+
+    print(len(df))
+    # print(df.columns.values)
 
     df.to_pickle('tweet_obj.pkl')
